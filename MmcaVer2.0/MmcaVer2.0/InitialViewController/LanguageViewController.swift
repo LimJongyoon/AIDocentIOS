d799e6ff9063263205498d5409a8103517e4907e
Limjongyoon <limjy.kor@gmail.com>
2024-04-17 17:24:01 +0900

    뷰컨트롤러 구조변경, 앱 마지막 데이터 기억
    관설정이랑 언어를 디폴드 값을 가지고 있고, 나중에 변경하게 바꿈, 초기화면에 추가하더라도 맨처음 앱시작할 때만 있으면되기 때문에 나중에 추가할수 있어서 없애고 설정에서 건들수 있도록 했음.

--- MmcaVer2.0/MmcaVer2.0/InitialViewController/LanguageViewController.swift
+++ MmcaVer2.0/MmcaVer2.0/InitialViewController/LanguageViewController.swift
@@ -1,29 +1,32 @@
-//
-//  LanguageViewController.swift
-//  MmcaVer2.0
-//
-//  Created by Lim on 4/15/24.
-//
-
 import UIKit
 
 class LanguageViewController: UIViewController {
-
+    
     
     
     @IBOutlet weak var koreaButton: UIButton!
     @IBOutlet weak var USAButton: UIButton!
     @IBOutlet weak var japanButton: UIButton!
+    @IBOutlet weak var languageLabel: UILabel!
+    
+    let lastSelectedLanguageKey = "lastSelectedLanguage"
+    let lastSelectedLabelTextKey = "lastSelectedLabelText"
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        // 이전에 저장된 마지막 선택한 언어와 해당 언어에 대한 라벨 불러오기
+        if let lastSelectedLanguage = UserDefaults.standard.string(forKey: lastSelectedLanguageKey),
+           let lastSelectedLabelText = UserDefaults.standard.string(forKey: lastSelectedLabelTextKey) {
+            updateTitleAndLabel(title: lastSelectedLanguage, text: lastSelectedLabelText)
+        }
+        
         // 모든 버튼의 테두리를 둥글게 설정하는 함수 호출
         configureButton(koreaButton, withBorderColor: UIColor.black.cgColor)
         configureButton(USAButton, withBorderColor: UIColor.black.cgColor)
         configureButton(japanButton, withBorderColor: UIColor.black.cgColor)
     }
-
+    
     // 버튼의 모양을 구성하는 함수
     func configureButton(_ button: UIButton, withBorderColor borderColor: CGColor) {
         button.layer.cornerRadius = 5  // 원하는 모서리의 반경 설정
@@ -33,15 +36,29 @@ class LanguageViewController: UIViewController {
     }
     
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // 각 버튼을 눌렀을 때 실행될 액션들
+    
+    @IBAction func koreaButton(_ sender: UIButton) {
+        updateTitleAndLabel(title: "언어", text: "한국어")
     }
-    */
-
+    
+    @IBAction func USAButton(_ sender: UIButton) {
+        updateTitleAndLabel(title: "Language", text: "English")
+    }
+    
+    @IBAction func japanButton(_ sender: UIButton) {
+        updateTitleAndLabel(title: "言語", text: "日本語")
+    }
+    
+    
+    // 타이틀과 라벨을 업데이트하는 함수
+    func updateTitleAndLabel(title: String, text: String ) {
+        self.navigationItem.title = title  // 뷰 컨트롤러의 타이틀 업데이트
+        languageLabel.text = text  // 라벨의 텍스트 업데이트
+        
+        // 선택한 언어와 해당 언어에 대한 라벨을 UserDefaults에 저장
+        UserDefaults.standard.set(title, forKey: lastSelectedLanguageKey)
+        UserDefaults.standard.set(text, forKey: lastSelectedLabelTextKey)
+    }
 }
+
--- MmcaVer2.0/MmcaVer2.0/InitialViewController/PlaceSelectViewController.swift
+++ MmcaVer2.0/MmcaVer2.0/InitialViewController/PlaceSelectViewController.swift
@@ -1,9 +1,4 @@
-//
-//  PlaceSelectViewController.swift
-//  MmcaVer2.0
-//
-//  Created by Lim on 4/15/24.
-//
+
 
 import UIKit
 
@@ -16,34 +11,50 @@ class PlaceSelectViewController: UIViewController {
     @IBOutlet weak var chungjuButton: UIButton!
     @IBOutlet weak var deoksugungButton: UIButton!
     
+    let lastSelectedTitleKey = "lastSelectedTitle"
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // 이전에 저장된 마지막 선택한 타이틀 불러오기
+        if let lastSelectedTitle = UserDefaults.standard.string(forKey: lastSelectedTitleKey) {
+            self.navigationItem.title = lastSelectedTitle
+        }
         
         configureButton(seoulButton, withBorderColor: UIColor.black.cgColor)
         configureButton(gwacheonButton, withBorderColor: UIColor.black.cgColor)
         configureButton(chungjuButton, withBorderColor: UIColor.black.cgColor)
         configureButton(deoksugungButton, withBorderColor: UIColor.black.cgColor)
-        
-        func configureButton(_ button: UIButton, withBorderColor borderColor: CGColor) {
-            button.layer.cornerRadius = 10  // 원하는 모서리의 반경 설정
-            button.layer.borderWidth = 0   // 테두리 두께 설정
-            button.layer.borderColor = borderColor  // 테두리 색상 설정
-            button.clipsToBounds = true  // 이 속성은 뷰의 콘텐츠가 뷰의 경계를 넘어가지 않도록 합니다.
-        }
-        
-        
-        // Do any additional setup after loading the view.
     }
     
+    func configureButton(_ button: UIButton, withBorderColor borderColor: CGColor) {
+        button.layer.cornerRadius = 10  // 원하는 모서리의 반경 설정
+        button.layer.borderWidth = 0   // 테두리 두께 설정
+        button.layer.borderColor = borderColor  // 테두리 색상 설정
+        button.clipsToBounds = true  // 이 속성은 뷰의 콘텐츠가 뷰의 경계를 넘어가지 않도록 합니다.
+    }
     
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
+    @IBAction func seoulButton(_ sender: UIButton) {
+        updateTitleWith("서울관")
+    }
+    
+    @IBAction func gwacheonButton(_ sender: UIButton) {
+        updateTitleWith("과천관")
+    }
+    
+    @IBAction func chungjuButton(_ sender: UIButton) {
+        updateTitleWith("청주관")
+    }
+    
+    @IBAction func deoksugungButton(_ sender: UIButton) {
+        updateTitleWith("덕수궁관")
+    }
+    
+    func updateTitleWith(_ title: String) {
+        self.navigationItem.title = title
+        // 선택한 타이틀을 UserDefaults에 저장
+        UserDefaults.standard.set(title, forKey: lastSelectedTitleKey)
+    }
     
 }
