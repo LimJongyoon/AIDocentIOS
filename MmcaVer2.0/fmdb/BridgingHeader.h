4e19d0adeb15f5a475061cadb61d56cfc43e7e0d
Limjongyoon <limjy.kor@gmail.com>
2024-05-16 16:49:21 +0900

    FMDB설정/비콘DB작성
    FMDB설정
    sqlite3추가해주고, FMDB 깃클론해서 object-C로 브릿징해줌
    비콘DB 만들어놓음

---
+++ MmcaVer2.0/fmdb/BridgingHeader.h
@@ -0,0 +1,13 @@
+//
+//  bridgingHeader.h
+//  MmcaVer2.0
+//
+//  Created by Lim on 5/16/24.
+//
+
+#ifndef BridgingHeader_h
+#define BridgingHeader_h
+
+
+#endif /* BridgingHeader_h */
+#import "FMDB.h"
