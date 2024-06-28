4e2cbadca21b184eb930f8d98324af88a68c008f
Limjongyoon <limjy.kor@gmail.com>
2024-06-28 15:14:36 +0900

    검색 수정중1
    디테일뷰 컨트롤러 안건드린버전

--- MmcaVer2.0/MmcaVer2.0/BoxTableViewController/InfoViewController.swift
+++ MmcaVer2.0/MmcaVer2.0/BoxTableViewController/InfoViewController.swift
@@ -15,28 +15,21 @@ class InfoViewController: UIViewController, CBCentralManagerDelegate, UITableVie
     
     // 디바이스 정보 데이터 구조
     public let deviceInfo: [String: (title: String, artist: String, size: String, material: String, description: String)] = [
-        
-        
         "MMCA001": ("우제류를 위하여",
                     "신현중(1995)",
                     "195x155x45mm",
                     "청동",
                     "신현중(1953- )의 <뿔 있는 우제류를 위하여>(1995)는 인류 문명의 원형에 대한 탐구를 우제류 연작을 통하여 형상화한 작품이다. 작가는 인류가 살기 좋은 조건이 되게끔 인간에게 고기와 젖, 뿔과 털 그리고 노동력까지 제공해 온 우제류 동물 중에서, 사슴, 산양, 영양, 가젤, 임팔라 등 7마리를 작품의 주제로 하고 있다. 이 작품은 오늘날 우제류 동물들의 인류를 위한 헌신에 감사하는 마음으로 제작된 것으로, 2년여에 걸쳐 나무로 원형을 만든 후 청동 주물로 뜨고 군집형태로 설치하여 더욱 현장감있게 주제를 전달하고 있다."),
-        
-        
         "MMCA002": ("마두",
                     "권진규(1952)",
                     "31.4×64.2×15.6mm",
                     "안산암",
                     "권진규(權鎭圭, 1922-1973)는 함경남도 함흥에서 태어났다. 1947-1948년경에 이쾌대가 운영하는 성북회화연구소에서 미술을 배웠으며 속리산 법주사 대불 제작에 참가하였다. 1948년에 일본으로 건너간 후 1949년 9월에 무사시노미술대학(武蔵野美術大学) 조각과에 입학하여 시미즈 다카시(淸水多嘉示, 1897-1981)를 사사했다. 1953년부터 1955년까지 니카텐(二科展, 이과전)에 말을 주제로 한 작품을 지속적으로 출품하였고, 《제38회 니카텐》(1953)에서 특대(特待)를 수상하였다. 1959년에 귀국 후 동선동에 정착하여 작업 활동을 시작하였다. 첫 개인전 《권진규 조각전》(1965)을 비롯하여 한국과 일본을 오가며 활동하였다. 권진규는 말, 여인의 누드, 인물 흉상과 두상 등을 소재로 했으며, 주로 석재, 테라코타(terra-cotta), 건칠, 목재 등의 재료를 사용하여 환조와 부조 형태의 작품을 제작했다."),
-        
-    
-        "MMCA003": ("트리", 
+        "MMCA003": ("트리",
                     "권오상(2013)",
                     "274x184x167mm",
                     "종이,에폭시,폴리스티렌",
                     "소조는 무언가를 붙여서 형태를 만드는 것이다. 따라서 사진을 붙여서 형태를 만드는 그의 ‘사진조각’도 어떤 의미에서는 소조에 해당된다. 2010년대에 들어 그는 사진조각에 여러 물건을 붙여 복잡한 구성을 만드는데, 이것도 소조의 개념이라고 볼 수 있다. 더불어 그러한 물건들은 모두 부피가 있는 ‘덩어리’들이다. 사실 조각에 있어 중요한 요소 중 하나가 덩어리이다.위와 같은 소조와 덩어리에 대한 그의 생각이 가장 잘 반영된 작품이 바로 <트리>이다. <트리>는 크리스마스트리를 소재로 한 작업이다. 보통 크리스마스트리에는 전등, 종, 공, 장식품 등 많은 소품들이 붙어 있다. 권오상은 이 소품들을 대신해 다른 물건들을 트리에 붙였다. 그 물건 중에는 예전 작업에 등장했던 물건과 사람도 있다. 작가는 로댕의 <지옥의 문>에서 바글거리는 것을 생각하며 이 작업을 만들었다고 한다."),
-        
         "우제류": ("유유유", "작가4(2023)", "000x000mm", "재료4", "설명입니다 설명입니다 설명입니다 설명입니다 설명입니다")
     ]
     
@@ -106,6 +99,7 @@ class InfoViewController: UIViewController, CBCentralManagerDelegate, UITableVie
     
     // UITableViewDataSource 프로토콜 메소드 - 주변 기기 수 반환
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // 허용된 이름을 가진 주변 기기의 수를 반환
         return discoveredPeripherals.filter { allowedDeviceNames.contains($0.name ?? "") }.count
     }
     
@@ -113,21 +107,25 @@ class InfoViewController: UIViewController, CBCentralManagerDelegate, UITableVie
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // 이름이 있는 주변 기기만 필터링
         let namedPeripherals = discoveredPeripherals.filter { allowedDeviceNames.contains($0.name ?? "") }
+        
         // 재사용 가능한 셀 가져오거나 없으면 새로운 셀 생성
         let cell = tableView.dequeueReusableCell(withIdentifier: "BLECell") ?? UITableViewCell(style: .subtitle, reuseIdentifier: "BLECell")
         
+        // 현재 인덱스에 해당하는 주변 기기 가져오기
         let peripheral = namedPeripherals[indexPath.row]
         
-        // 주변 기기의 이름 및 RSSI 값 설정
+        // 주변 기기의 이름을 가져옴
         let peripheralName = peripheral.name ?? ""
         
+        // 디바이스 정보에 해당 주변 기기의 정보가 있는지 확인
         if let info = deviceInfo[peripheralName] {
-            // 텍스트 라벨 설정
-            cell.textLabel?.text = info.title
-            cell.detailTextLabel?.text = "\(info.artist), \(info.size), \(info.material)"
+            // 정보가 있는 경우 셀의 텍스트 라벨과 세부 텍스트 라벨 설정
+            cell.textLabel?.text = info.title // 작품의 제목 설정
+            cell.detailTextLabel?.text = "\(info.artist), \(info.size), \(info.material)" // 작가, 크기, 재질 정보를 설정
         } else {
-            cell.textLabel?.text = peripheralName
-            cell.detailTextLabel?.text = "RSSI: \(rssiValues[peripheral] ?? 0)"
+            // 정보가 없는 경우 기본 텍스트 설정
+            cell.textLabel?.text = peripheralName // 주변 기기의 이름 설정
+            cell.detailTextLabel?.text = "RSSI: \(rssiValues[peripheral] ?? 0)" // RSSI 값을 표시
         }
         
         // 이미지 파일을 Assets에서 찾기
@@ -157,8 +155,8 @@ class InfoViewController: UIViewController, CBCentralManagerDelegate, UITableVie
         // 선택된 주변 기기 정보 전달
         let namedPeripherals = discoveredPeripherals.filter { allowedDeviceNames.contains($0.name ?? "") }
         let selectedPeripheral = namedPeripherals[indexPath.row]
-        detailVC.peripheral = selectedPeripheral
-        detailVC.rssiValue = rssiValues[selectedPeripheral] ?? 0
+        detailVC.peripheral = selectedPeripheral // 선택된 주변 기기를 DetailViewController에 전달
+        detailVC.rssiValue = rssiValues[selectedPeripheral] ?? 0 // 선택된 주변 기기의 RSSI 값을 전달
         detailVC.deviceInfo = deviceInfo // deviceInfo 딕셔너리를 DetailViewController로 전달
         
         // DetailViewController로 전환
--- MmcaVer2.0/MmcaVer2.0/BoxTableViewController/SearchViewController.swift
+++ MmcaVer2.0/MmcaVer2.0/BoxTableViewController/SearchViewController.swift
@@ -1,84 +1,142 @@
 import UIKit
 
-class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
-    
-    let tableView = UITableView()
-    let searchBar = UISearchBar()
-    var filteredDeviceInfo: [String: (title: String, artist: String, size: String, material: String, description: String)] = [:]
+class SearchViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
+    var tableView = UITableView()
+    var searchBar = UISearchBar()
+    var filteredDeviceInfo: [(key: String, value: (title: String, artist: String, size: String, material: String, description: String))] = []
+    var deviceInfo: [String: (title: String, artist: String, size: String, material: String, description: String)] = [:]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .white
+        // 배경색을 흰색으로 설정
+        self.view.backgroundColor = .white
         
-        let titleLabel = UILabel()
-        titleLabel.text = "작품 검색하기"
-        titleLabel.textAlignment = .left
-        titleLabel.font = UIFont.systemFont(ofSize: 30, weight: .bold)
-        titleLabel.frame = CGRect(x: 20, y: 100, width: self.view.bounds.width, height: 50)
-        view.addSubview(titleLabel)
+        // "작품 검색하기" 레이블 설정
+        let searchLabel = UILabel()
+        searchLabel.text = "작품 검색하기"
+        searchLabel.textAlignment = .left
+        searchLabel.font = UIFont.systemFont(ofSize: 30, weight: .bold)
+        searchLabel.frame = CGRect(x: 20, y: 100, width: self.view.bounds.width, height: 50)
+        self.view.addSubview(searchLabel)
         
-        searchBar.delegate = self
+        // 검색 바 설정
         searchBar.frame = CGRect(x: 0, y: 160, width: self.view.bounds.width, height: 50)
-        view.addSubview(searchBar)
+        searchBar.delegate = self
+        self.view.addSubview(searchBar)
         
+        // 테이블 뷰 설정
         tableView.frame = CGRect(x: 0, y: 220, width: self.view.bounds.width, height: self.view.bounds.height - 220)
+        tableView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         tableView.delegate = self
         tableView.dataSource = self
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "SearchCell")
-        view.addSubview(tableView)
+        self.view.addSubview(tableView)
+        
+        // 디바이스 정보를 설정
+        deviceInfo = [
+            "MMCA001": ("우제류를 위하여", "신현중(1995)", "195x155x45mm", "청동", "신현중(1953- )의 <뿔 있는 우제류를 위하여>(1995)는 인류 문명의 원형에 대한 탐구를 우제류 연작을 통하여 형상화한 작품이다."),
+            "MMCA002": ("마두", "권진규(1952)", "31.4×64.2×15.6mm", "안산암", "권진규(권진규, 1922-1973)는 함경남도 함흥에서 태어났다."),
+            "MMCA003": ("트리", "권오상(2013)", "274x184x167mm", "종이,에폭시,폴리스티렌", "소조는 무언가를 붙여서 형태를 만드는 것이다."),
+            "우제류": ("유유유", "작가4(2023)", "000x000mm", "재료4", "설명입니다 설명입니다 설명입니다 설명입니다 설명입니다")
+        ]
+        
+        // 필터된 데이터 초기화
+        filteredDeviceInfo = Array(deviceInfo)
+        
+        // 화면 탭 제스처 추가
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        tapGesture.cancelsTouchesInView = false
+        self.view.addGestureRecognizer(tapGesture)
     }
     
+    // 키보드 숨기기 메소드
+    @objc func dismissKeyboard() {
+        searchBar.resignFirstResponder()
+    }
+    
+    // UISearchBarDelegate 메소드 - 검색 바 텍스트가 변경될 때 호출
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        guard !searchText.isEmpty else {
-            filteredDeviceInfo.removeAll()
-            tableView.reloadData()
-            return
+        filterContentForSearchText(searchText)
+    }
+    
+    // UISearchBarDelegate 메소드 - 검색 버튼 클릭 시 호출
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+    }
+    
+    // 검색 텍스트에 따라 데이터 필터링
+    func filterContentForSearchText(_ searchText: String) {
+        if searchText.isEmpty {
+            filteredDeviceInfo = Array(deviceInfo)
+        } else {
+            filteredDeviceInfo = deviceInfo.filter {
+                $0.value.title.contains(searchText) ||
+                $0.value.artist.contains(searchText) ||
+                $0.value.size.contains(searchText) ||
+                $0.value.material.contains(searchText)
+            }
         }
-        
-        filteredDeviceInfo = InfoViewController().deviceInfo.filter { key, value in
-            return key.contains(searchText) ||
-                   value.title.contains(searchText) ||
-                   value.artist.contains(searchText) ||
-                   value.description.contains(searchText)
-        }
-        
         tableView.reloadData()
     }
     
+    // UITableViewDataSource 메소드 - 섹션 수 반환
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
     
+    // UITableViewDataSource 메소드 - 행 수 반환
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return filteredDeviceInfo.count
     }
     
+    // UITableViewDataSource 메소드 - 셀 반환
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "SearchCell", for: indexPath)
+        let device = filteredDeviceInfo[indexPath.row]
         
-        let key = Array(filteredDeviceInfo.keys)[indexPath.row]
-        let info = filteredDeviceInfo[key]!
+        let highlightedText = searchBar.text ?? ""
         
-        cell.textLabel?.text = info.title
-        cell.detailTextLabel?.text = "\(info.artist), \(info.size), \(info.material)"
+        // 타이틀에 강조 표시 추가
+        let attributedTitle = NSMutableAttributedString(string: device.value.title)
+        let titleRange = (device.value.title as NSString).range(of: highlightedText)
+        attributedTitle.addAttribute(.backgroundColor, value: UIColor.yellow, range: titleRange)
         
-        var image: UIImage? = UIImage(named: key)
+        // 작가에 강조 표시 추가
+        let attributedArtist = NSMutableAttributedString(string: device.value.artist)
+        let artistRange = (device.value.artist as NSString).range(of: highlightedText)
+        attributedArtist.addAttribute(.backgroundColor, value: UIColor.yellow, range: artistRange)
         
+        // 셀의 텍스트 라벨 설정
+        cell.textLabel?.attributedText = attributedTitle
+        cell.detailTextLabel?.attributedText = attributedArtist
+        
+        // 이미지 파일을 Assets에서 찾기
+        var image: UIImage? = UIImage(named: device.key)
+        
+        // 이미지가 없을 경우, 시스템의 기본 이미지 사용
         if image == nil {
             image = UIImage(named: "EmptyQuestionMark")
         }
         
+        // 셀의 왼쪽에 이미지뷰 추가
         cell.imageView?.image = image
         
         return cell
     }
     
+    // UITableViewDelegate 메소드 - 셀 높이 반환
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    
+    // UITableViewDelegate 메소드 - 셀 선택 시 호출
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let detailVC = DetailViewController()
-        
-        let key = Array(filteredDeviceInfo.keys)[indexPath.row]
-        detailVC.deviceInfo = [key: filteredDeviceInfo[key]!]
+        let selectedDevice = filteredDeviceInfo[indexPath.row]
+        detailVC.peripheral = nil
+        detailVC.rssiValue = nil
+        detailVC.deviceInfo = deviceInfo
         
         self.navigationController?.pushViewController(detailVC, animated: true)
     }
